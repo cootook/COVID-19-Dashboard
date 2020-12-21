563b1b5bf4776fdb4763c28d83f2d46a2abfbbbe
friedooo <friedlich3@gmail.com>
2020-12-22 00:28:15 +0300

    feat: add graph maker

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // import testTest from './scripts/script.test.js';
 import getDataFromApiStat from './scripts/get-data-from-api-stat.js'
 import dataApiDiseaseSh from './data/from-api-disease-sh.js';
+import Graph from './scripts/chart.js';
 
 
 // console.log('test 1');
@@ -9,3 +10,10 @@ getDataFromApiStat().then((r) => console.log('main --- ', dataApiDiseaseSh, 'res
 // setTimeout(() => {
 //   console.log('main --- ', dataApiDiseaseSh)
 // }, 2000)
+
+
+let x = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+let values = [10,9,8,7,6,5,4,3,2,1];
+let graph = new Graph();
+graph.initSize();
+graph.createGraph(x, values, 'bar', 'daily cases');
