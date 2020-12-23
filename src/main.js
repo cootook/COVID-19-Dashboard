798c40f24ce3889a7813401d36defa1830211f8b
friedooo <friedlich3@gmail.com>
2020-12-23 22:47:35 +0300

    feat: dailyHandler

--- src/main.js
+++ src/main.js
@@ -5,15 +5,15 @@ import Graph from './scripts/chart.js';
 const grafCovid = (dataObj) => {
   let buttons = document.querySelectorAll('.graph-container__buttons > button');
   let arrName = ['Cumulative Cases', 'Cumulative Deaths', 'Cumulative Recovers'];
-  let arrUrl = ['cases', 'deaths', 'recovered'];
+  let arrData = ['cases', 'deaths', 'recovered', 'cases', 'deaths', 'recovered'];
   let index = 0;
 
   let values;
   let data;
   let x;
 
-  function dataHandler() {
-    data = dataObj[arrUrl[index]];
+  function cumulativeHandler() {
+    data = dataObj[arrData[index]];
     x = [];
     values = [];
     for (let elem in data) {
@@ -21,8 +21,35 @@ const grafCovid = (dataObj) => {
       values.push(data[elem]);
     }
   }
+
+  function dailyHandler() {
+    data = dataObj[arrData[index]];
+    x = [];
+    values = [];
+    for (let elem in data) {
+      x.push(elem);
+      values.push(data[elem]);
+    }
+    x.shift();
+
+    console.log(values);
+
+    let newValues = [];
+
+    for (let i = 1; i < values.length; i += 1) {
+      newValues[i] = values[i] - values[i - 1];
+    }
+
+    console.log(newValues);
+  }
  
-  dataHandler();
+  cumulativeHandler();
+  //dailyHandler();
+
+  //console.log(x);
+  //console.log(values);
+  //console.log(index);
+
   let graph = new Graph();
   graph.initSize();
   graph.createGraph(x, values, 'line', arrName[index]);
@@ -47,7 +74,7 @@ const grafCovid = (dataObj) => {
 
     document.querySelector('.graph-container__title').innerHTML = arrName[index];
     
-    dataHandler();
+    cumulativeHandler()
     let graph = new Graph();
     graph.initSize();
     graph.createGraph(x, values, 'line', arrName[index]);
@@ -57,14 +84,13 @@ const grafCovid = (dataObj) => {
     e.addEventListener('click', buttonHandler);
   })
     
- 
 }
 
 async function mainCovid() {
   // here put functions
   await getDataFromApiStat() // this must be before you take data from  {dataApiDiseaseSh}!
-  console.log('mainCovid --->  dataApiDiseaseSh', dataApiDiseaseSh.worldHistory)
-  await grafCovid(dataApiDiseaseSh.worldHistory);
+  console.log('mainCovid --->  dataApiDiseaseSh', dataApiDiseaseSh)
+  grafCovid(dataApiDiseaseSh, 'italia');
 }
 
 mainCovid();
