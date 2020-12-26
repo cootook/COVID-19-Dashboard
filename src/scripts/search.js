e3fd40e26d1487a23e2570887d77820715922ed6
friedooo <friedlich3@gmail.com>
2020-12-26 18:12:24 +0300

    fix: input event add keyup delete

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -2,7 +2,7 @@ export default function Search() {
     let input = document.querySelector('.list-container--search');
     let itemsList = [...document.querySelector('.list-container > ul').children];
 
-    input.addEventListener('keyup', function(e) {
+    input.addEventListener('input', function(e) {
         e.preventDefault();
         itemsList.forEach(e => {
             if (e.dataset.country.slice(0,this.value.length).toLowerCase() != this.value.toLowerCase()) {
