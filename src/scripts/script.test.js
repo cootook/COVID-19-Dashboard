54c48fef40585564a8748e55162e6d87f5232e3b
cootook <cootook@gmail.com>
2020-12-28 15:26:37 +0300

    refactor: delete file of test

--- src/scripts/script.test.js
+++
@@ -1,6 +0,0 @@
-export default function testTest(test = 'testing test') {
-  const url = 'https://jsonplaceholder.typicode.com/users';
-  return fetch(url).then((response) => response.text());
-
-  console.log(test);
-}
