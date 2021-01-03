19ce15fb072ad26ae7bfc91d77a70c3320d1c478
cootook <cootook@gmail.com>
2021-01-03 19:09:49 +0300

    feat: build bundle

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ function setDMode() {
 
 const config = {
   target: "web",
-  entry: {index: './js/main.js'},
+  entry: { index: './src/main.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -35,112 +35,113 @@ const config = {
   devtool: setDevTool(),
   module: {
     rules: [{
-        test: /\.html$/,
-        use: [{
-          loader: 'html-loader',
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: {
+          minimize: false
+        }
+      }]
+    },
+    {
+      test: /\.js$/,
+      use: ['babel-loader'/* , 'eslint-loader' */],
+      exclude: [
+        /node_modules/
+      ]
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
           options: {
-            minimize: false
+            sourceMap: true
           }
-        }]
-      },
-      {
-        test: /\.js$/,
-        use: ['babel-loader'/* , 'eslint-loader' */],
-        exclude: [
-          /node_modules/
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          }, {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
+        },
+      ]
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true
           }
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|svg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'img',
-              name: '[name].[ext]'
-            }},
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug : true,
-              mozjpeg: {
-                progressive: true,
-                quality: 75
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-                optimizationLevel: 1
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75
-              }
-            }
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true
           }
-        ]
-      },
-      {
-        test: /\.(woff|woff2|ttf|otf|eot)$/,
-        use: [{
+        }
+      ]
+    },
+    {
+      test: /\.(jpe?g|png|svg|gif)$/,
+      use: [
+        {
           loader: 'file-loader',
           options: {
-            outputPath: 'fonts'
+            outputPath: 'img',
+            name: '[name].[ext]'
           }
-        }]
-      }
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            bypassOnDebug: true,
+            mozjpeg: {
+              progressive: true,
+              quality: 75
+            },
+            // optipng.enabled: false will disable optipng
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: [0.65, 0.90],
+              speed: 4
+            },
+            gifsicle: {
+              interlaced: false,
+              optimizationLevel: 1
+            },
+            // the webp option will enable WEBP
+            webp: {
+              quality: 75
+            }
+          }
+        }
+      ]
+    },
+    {
+      test: /\.(woff|woff2|ttf|otf|eot)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          outputPath: 'fonts'
+        }
+      }]
+    }
     ]
   },
 
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css',
+      filename: './style.css',
     }),
     new HtmlWebPackPlugin({
-      template: 'index.html',
+      template: './src/index.html',
       filename: './index.html'
     }),
     new CopyWebpackPlugin([
       // {from: './src/static', to: './'},
-      // {from: './src/img', to: './img/'},
+      { from: './src/assets', to: './assets/' },
     ]),
   ],
 
